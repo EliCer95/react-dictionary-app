df19c51b44a6cadbdf2a2382295867129a6cc592
EliCer95 <89043035+EliCer95@users.noreply.github.com>
2023-10-19 17:21:45 +0200

    Integrated Dictionary API

--- src/App.js
+++ src/App.js
@@ -4,7 +4,15 @@ import Dictionary from "./Dictionary";
 export default function App() {
   return (
     <div className="App">
-      <Dictionary />
+      <header>
+        <h1 className="mt-5">Dictionary App</h1>
+      </header>
+      <div className="container">
+        <Dictionary />
+      </div>
+      <footer className="mt-5">
+        This app was made by Eliska Cerna and is open-sourced on GitHub
+      </footer>
     </div>
   );
 }
--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,13 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 
 export default function Dictionary() {
-  return (
-    <div className="Dictionary">
-      <h1>Dictionary App</h1>
-      <form>
-        <input type="search" />
-        <input type="submit" value="search" />
-      </form>
-    </div>
-  );
+  const [word, setWord] = useState("");
+  const [result, setResult] = useState("");
+  const [loaded, setLoaded] = useState(false);
+
+  function handleResponse(response) {
+    setResult(response.data[0]);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`;
+    axios.get(apiUrl).then(handleResponse);
+    setLoaded(true);
+  }
+
+  function handleChange(event) {
+    setWord(event.target.value);
+  }
+
+  if (loaded) {
+    return (
+      <div className="Dictionary">
+        <form
+          className="mt-4 mb-4 d-flex justify-content-center"
+          onSubmit={handleSubmit}
+        >
+          <input type="search" onChange={handleChange} />
+          <input
+            type="submit"
+            value="search"
+            className="btn btn-primary ms-4"
+          />
+        </form>
+        <h1>{result.word}</h1>
+        <h2>/ˈ{result.phonetic}/</h2>
+        <h3>Synonyms</h3>
+      </div>
+    );
+  } else {
+    return (
+      <div className="Dictionary">
+        <form
+          className="mt-4 mb-4 d-flex justify-content-center"
+          onSubmit={handleSubmit}
+        >
+          <input type="search" onChange={handleChange} />
+          <input
+            type="submit"
+            value="search"
+            className="btn btn-primary ms-4"
+          />
+        </form>
+      </div>
+    );
+  }
 }
