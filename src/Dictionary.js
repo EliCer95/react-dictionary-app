225a8680de820bfc69be574c97c30460bc56e2ae
EliCer95 <89043035+EliCer95@users.noreply.github.com>
2023-10-21 13:26:59 +0200

    Added CSS styling

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -36,11 +36,18 @@ export default function Dictionary(props) {
     return (
       <div className="Dictionary">
         <section>
+          <h2>What word do you want to look up?</h2>
           <form
             className="mt-4 mb-2 d-flex justify-content-center"
             onSubmit={handleSubmit}
           >
-            <input type="search" onChange={handleChange} />
+            <input
+              type="search"
+              onChange={handleChange}
+              placeholder={props.keyword}
+              autofocus={true}
+              className="form-control search-input"
+            />
           </form>
           <div className="hints">suggested words: sunset, wine, yoga,..</div>
         </section>
